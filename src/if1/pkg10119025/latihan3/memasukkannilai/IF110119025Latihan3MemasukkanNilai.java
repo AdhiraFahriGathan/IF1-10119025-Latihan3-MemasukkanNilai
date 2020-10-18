54533d732be02c3bfcf48bfa9611958187a549d3
unknown <adhiragathan12@gmail.com>
2020-10-18 20:15:29 +0700

    initial commit

---
+++ src/if1/pkg10119025/latihan3/memasukkannilai/IF110119025Latihan3MemasukkanNilai.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119025.latihan3.memasukkannilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class IF110119025Latihan3MemasukkanNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+    }
+    
+}
